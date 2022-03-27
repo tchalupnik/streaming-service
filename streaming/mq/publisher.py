c73df019bf0012e9a41a491d4c0a625445ae62ce
mistillate <tchalupnik@gmail.com>
2022-03-27 20:06:23 +0200

    MQPublisher more elegant way of setting up channel

--- streaming/mq/publisher.py
+++ streaming/mq/publisher.py
@@ -8,16 +8,20 @@ from streaming.publisher import BasePublisher, BasePublisherMessage
 from .base import MQBase
 from .factories import MQChannelFactory
 from .exceptions import MQError
+from .settings import MQSettings
 
 logger = logging.getLogger(__name__)
 
 
 class MQPublisher(MQBase, BasePublisher):
     routing_key: str
-    channel: AbstractRobustChannel
+
+    def __init__(self, settings: MQSettings = None, channel: AbstractRobustChannel = None):
+        super().__init__(settings)
+        self.channel = channel
 
     async def publish(self, message: BasePublisherMessage):
-        if not hasattr(self, "channel"):
+        if not self.channel:
             self.channel = await MQChannelFactory.get_publisher_channel()
 
         exchange = await self.channel.get_exchange(self.settings.DEFAULT_EXCHANGE_NAME)
